ff3a527a48e089982d60d06ce03e3129729b8ccb
Pierre Ingram <mr.pjingram@gmail.com>
2025-07-18 16:49:00 +0000

    Updated userRoutes, adding roles and validation to Login/Registration pages

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,8 @@ const userController = require('../controllers/userController.js');
 const userRouter = express();
 
 userRouter.get('/volunteers', userController.getAllVolunteers);
+userRouter.post('/login', userController.loginUser);
+userRouter.post('/register', userController.registerUser);
 
 module.exports = {
     userRouter
